05e7cd0d81e0739d618bc32d24b4d542d43f0ed8
WeiLee <weilee@WeiLees-Laptop.attlocal.net>
2024-06-04 15:24:19 -0700

    updte

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -4,14 +4,7 @@ import Card from "./Card";
 const CardList = ({ robots }) => {
   const cardComponent = robots.map((robot, i) => {
     const { id, name, email } = robot;
-    return (
-        <Card 
-            key={i} 
-            id={id} 
-            email={email} 
-            name={name} 
-        />
-    );
+    return <Card key={i} id={id} email={email} name={name} />;
   });
   return <div>{cardComponent}</div>;
 };
